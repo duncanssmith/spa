efbedef42ec8891adea967a05b9ae2cba7cf70de
Duncan Smith <duncanssmith@gmail.com>
2013-10-17 21:01:27 +0100

    Develop to chapter 6

--- js/spa.js
+++ js/spa.js
@@ -12,7 +12,10 @@
 /*global $, spa */
 
 var spa = (function () {
+  'use strict';
   var initModule = function ( $container ) {
+    spa.data.initModule();
+    spa.model.initModule();
     spa.shell.initModule( $container );
   };
 
